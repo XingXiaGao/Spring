45a6465d53135b7793128580eae554cb1e6cad42
高星霞 <201711010420@stu.sdnu.edu.cn>
2018-10-19 15:04:01 +0800

    Update 2-4.cpp

--- chapt_2_demo/2-4.cpp
+++ chapt_2_demo/2-4.cpp
@@ -3,12 +3,9 @@ using namespace std;
 
 float checkAgeScore(int age, float score)
 {
-	if (age < 16 || age>25)
-		throw age;
-	if (score < 0 || score>5)
-		throw score;
-	else
-		return score * 20;
+	if (age < 16 || age>25)   throw age;
+	if (score < 0 || score>5) throw score;
+	return score * 20;
 }
 int main()
 {
